9d1f949e5040b169b658b3545cd940397b2c6d08
Jacek Danecki <jacek.m.danecki@gmail.com>
2026-01-11 13:37:54 +0100

    fix lines.sh

--- lines.sh
+++ lines.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find . -name *.cpp -o -name *.h  -exec wc -l {} \; | awk '{s+=$$1} END { print s} '
+find . \( -name "*.cpp" -o -name "*.h" -o -name "*.inl" \) -print0 \
+| xargs -0 awk 'END {print NR}'
+
