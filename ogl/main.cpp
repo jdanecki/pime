1187378aa34d1cd49c179a2d3d0d423982e40ce4
Piotr Danecki <i3riced@mailfence.com>
2026-01-27 16:20:35 +0100

    ogl: cleanup surface after loading texture
    Signed-off-by: Piotr Danecki <i3riced@mailfence.com>

--- ogl/main.cpp
+++ ogl/main.cpp
@@ -48,17 +48,22 @@ GLuint load_texture(const char * filename)
     glActiveTexture(GL_TEXTURE0);
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+
     PFNGLGENERATETEXTUREMIPMAPPROC glGenerateMipmap = (PFNGLGENERATETEXTUREMIPMAPPROC)SDL_GL_GetProcAddress("glGenerateMipmap");
     glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
     glBindTexture(GL_TEXTURE_2D, 0);
+
     glDisable(GL_TEXTURE_2D);
+    SDL_DestroySurface(surface);
     return texture;
 }
 
